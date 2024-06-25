2cdce2edf1251c28e8da0cac9867dcc15347200d
GannaFilch <ganna.filch@gmail.com>
2024-06-25 13:57:04 +0300

    homework

---
+++ Week4/Day 1/ExerciseXP/Ex1.sql
@@ -0,0 +1,59 @@
+-- create table items(
+-- 	id serial primary key,
+-- 	item_name varchar(20),
+-- 	price float
+-- );
+
+-- create table customers(
+-- 	id serial primary key,
+-- 	first_name varchar(20),
+-- 	last_name varchar(20)
+-- );
+
+-- insert into items(
+-- 	item_name, price) values (
+-- 	'Small Desk',
+-- 	100
+-- 	),
+-- 	(
+-- 	'Large Desk',
+-- 	300
+-- 	),
+-- 	(
+-- 	'Fan',
+-- 	80
+-- 	);
+
+-- insert into customers(
+-- 	first_name, last_name) values (
+-- 	'Greg',
+-- 	'Jones'
+-- 	),
+-- 	(
+-- 	'Sandra',
+-- 	'Jones'
+-- 	),(
+-- 	'Scott',
+-- 	'Scott'
+-- 	),
+-- 	(
+-- 	'Trevor',
+-- 	'Green'
+-- 	),
+-- 	(
+-- 	'Melanie',
+-- 	'Johnson'
+-- 	);
+
+-- select * from items
+
+-- select item_name from items where price > 80
+
+-- select item_name from items where price <= 300
+
+-- select first_name from customers where last_name = 'Smith'  - null
+
+-- select first_name from customers where last_name = 'Jones'
+
+-- select * from customers where first_name != 'Scott'
+
